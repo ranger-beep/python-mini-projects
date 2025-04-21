ee8d3da14bcbcd7364e622b386bc827cf677dc46
ranger-beep <rnggry@gmail.com>
2025-04-22 01:07:53 +0200

    Create odd_even_checker.py

---
+++ odd-even-checker/odd_even_checker.py
@@ -0,0 +1,7 @@
+print("Selamat datang di pengukur Ganjil atau Genap")
+number = int(input("Masukkan angka yang mau di cek : "))
+
+if number % 2 == 0:
+    print("Type number: Genap")
+else:
+    print("Type Number: Ganjil")
