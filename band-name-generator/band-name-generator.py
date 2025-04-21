fe9f7ed038bcd58260a87b6750ce99559f5634bf
ranger-beep <rnggry@gmail.com>
2025-04-21 21:26:42 +0200

    Create band-name-generator.py

---
+++ band-name-generator/band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Generator")
+nama_kota = input("Kamu dibesarkan di kota bernama apa? ")
+nama_pet = input("Apa nama hewan peliharaanmu? ")
+print("Nama band yang cocok untukmu adalah " + nama_kota + " " + nama_pet)
